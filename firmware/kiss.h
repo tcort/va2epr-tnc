1683d45dfa84b8140d9fc3d559ca8524ee3b31a3
Thomas Cort <linuxgeek@gmail.com>
2012-09-10 06:13:23 -0400

    Add TX/RX buffers

--- firmware/kiss.h
+++ firmware/kiss.h
@@ -1,6 +1,13 @@
 #ifndef __KISS_H
 #define __KISS_H
 
+#define TX_BUFFER_SIZE (16384)
+#define RX_BUFFER_SIZE (16384)
+
+/*
+ * Special KISS Bytes
+ */
+
 /* Frame End */
 #define KISS_FEND (0xc0)
 
