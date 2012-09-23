0c710958b0f68167664cc0e434aba202940c4e37
Thomas Cort <linuxgeek@gmail.com>
2012-09-23 19:52:50 -0400

    Implement experimental RX (untested).

--- firmware/afsk.c
+++ firmware/afsk.c
@@ -7,15 +7,18 @@
  * a given tolerance. Can output waveforms at 1200 Hz and 2200 Hz. This is
  * enough to show that the circuits used in the schematic are viable.
  *
- * The code needs to be enhanced to capture incoming data.
+ * Code to capture incoming data and output it via USART0 is written
+ * but untested.
+ *
  * The code needs to be enhanced to output data from a buffer. This can probably be
- * done by using Timer 0 or Timer 3 to periodically sample data or periodically
+ * done by using Timer 3 to periodically sample data or periodically
  * change afsk_output_frequency to TONE_1200HZ or TONE_2200HZ. When in TX mode,
  * consider using a watchdog timer to help prevent getting stuck in TX mode.
  *
  * Ports and Peripherals Used
  * --------------------------
  *
+ * Timer 0 (AFSK Decoding)
  * Timer 1 (Input Capture)
  * Timer 2 (Waveform Generation)
  *
@@ -28,7 +31,9 @@
 #include <avr/io.h>
 
 #include "afsk.h"
+#include "ax25.h"
 #include "csma.h"
+#include "kiss.h"
 
 /*
  * For a 32 sample 2200 Hz tone, I need to change the output 70400
@@ -58,6 +63,14 @@
 #define PERIOD_1200_MAX 2632 /* 0.7 kHz */
 
 /*
+ * This is the counter value for the AFSK decoding interrupt
+ * running on Timer 0.
+ *
+ * 14745600/8/9600 - 1 = 191
+ */
+#define TIMER_9600 191 /* 9600 Hz */
+
+/*
  * This is the value that is used to set OCR2A. It controls the
  * output frequency when in transmit mode by setting the correct
  * period. Set it to TONE_1200HZ or TONE_2200HZ.
@@ -85,6 +98,14 @@ unsigned char sinewave[32] = {
  * TODO I updated the table from 16 samples to 32.
  * I need to see how this looks on a scope.
  */
+
+/*
+ * House keeping for the RX code. This flag used to
+ * indicate that a start-of-frame KISS_FEND has been
+ * sent via USART0, I am sending an AX25 frame, and
+ * a KISS_FEND will be needed to complete the frame.
+ */
+unsigned char in_kiss_frame = 0;
 
 
 /* timer1 input capture housekeeping */
@@ -118,6 +139,15 @@ void afsk_init(void) {
 	TCCR1A = 0x00; /* Normal Mode */
 	TCCR1B |= ((1<<ICNC1)|(1<<ICES1)|(1<<CS11)); /* noise canceler, rising edge, pre-scalar = 8 */
 
+	/* -- AFSK Decoding -- */
+	/* do all setup except enabling of afsk decoding interrupts (done in rx()) */
+
+	/* Timer 0 CTC, pre-scalar 8 */
+	TCCR0A |= (1<<WGM01); /* CTC */
+	TCCR0B |= (1<<CS01); /* pre-scalar = 8 */
+	TCNT0 = 0x00; /* initialize counter to 0 */
+	OCR0A = TIMER_9600; /* 9600 times per second */
+
 	/* -- Push to Talk -- */
 	/* setup DDR and turn PTT OFF */
 
@@ -132,10 +162,25 @@ void afsk_init(void) {
  */
 void tx(void) {
 
+	/* Turn off RX interrupts */
+	TIMSK0 &= ~(1<<OCIE0A); /* disable afsk decoder */
 	TIMSK1 &= ~((1<<ICIE1)|(1<<TOIE1)); /* disable overflow and input capture interrupts */
 
+	/*
+	 * If it goes into TX without finishing receiving the current AX.25 frame,
+	 * then it must be cleaned up and an ending KISS_FEND must be sent. This
+         * situation could happen (maybe) if the sending station drops out or
+	 * something else goes wrong.
+	 */
+	if (in_kiss_frame) {
+		/* End the current KISS frame. */
+		kiss_tx(AX25_FLAG);
+		kiss_tx_raw(KISS_FEND);
+		in_kiss_frame = 0;
+	}
+
+	/* Turn on TX interrupts/pins */
 	PORTD |= (1<<PD7); /* PTT ON */
-	/* TODO do I want a delay here? */
 	TIMSK2 |= (1<<OCIE2A); /* enable compare match interrupt */
 }
 
@@ -144,11 +189,195 @@ void tx(void) {
  */
 void rx(void) {
 
+	/* Turn off TX interrupts/pins */
 	TIMSK2 &= ~(1<<OCIE2A); /* disable compare match interrupt */
-	/* TODO do I want a delay here? */
 	PORTD &= ~(1<<PD7); /* PTT OFF */
 
+	/* Turn on RX interrupts */
 	TIMSK1 |= ((1<<ICIE1)|(1<<TOIE1)); /* enable overflow and input capture interrupts */
+	TIMSK0 |= (1<<OCIE0A); /* enable afsk decoder */
+}
+
+/*
+ * The algorithm used to receive and decode the AFSK NRZI encoded
+ * data is not something I came up with. Several projects make use
+ * of it. I'm having a hard time tracking down the originator to give
+ * him or her the proper credit. I first read about the algorithm on
+ * Gary N. Dion's WhereAVR project page: http://garydion.com/projects/whereavr/
+ * Gary links to Mike Berg's Zero Cross Detector decoder project page
+ * http://www.ringolake.com/pic_proj/zcd/zcdmodem.html Mike's site
+ * links to Bob Ball's QEX Article "WB8WGA's Simple, inexpensive ($25) TNC"
+ * http://www.n9cx.net/BobBall.htm
+ *
+ * Author's note: none of the code in this project is directly copied
+ * from any of the above sources, but I did implement it using their
+ * descriptions of the zero cross detector decoding algorithm with
+ * some changes to suit this project's needs and hardware.
+ */
+
+/*
+ * decode incoming AFSK data, forward it to the computer
+ * over USART0 using the KISS protocol.
+ *
+ * There are some design decisions made which impact this code.
+ * I could write the bytes to a buffer and CRC the frames before
+ * sending them to the computer. Instead I chose to forward each
+ * byte as it comes in. This saves CPU time and memory on the
+ * microcontroller at the expense of forwarding some possibly
+ * invalid frames to the computer (which should do it's own CRC
+ * checks on the incoming frames anyway).
+ */
+ISR(TIMER0_COMPA_vect) {
+
+	/* receive buffer holding the last 8 received bits */
+	static unsigned char bits = 0x00;
+
+	/* counter used to track the number of bits in 'bits' */
+	static unsigned char bits_count = 0;
+
+	/* count the 1's received - used for AX.25 bit stuffing */
+	static unsigned char ones_count = 0;
+
+	/* pseudo-timer used to decide when to sample */
+	static unsigned char sample_timer = 1;
+
+	/* is a signal being received? */
+	if (carrier_sense) {
+
+		/*
+		 * synchronize the sample_timer on shift_detect; sample when the time comes.
+		 */
+		if (shift_detect) {
+
+			/*
+			 * Check for AX.25 bit stuffing. If stuffing, don't sample the '0'.
+			 *
+			 * From the AX.25 spec (v2.2):
+			 *
+			 *	"any time five contiguous “1” bits are received, a “0” bit
+			 *	immediately following five “1” bits is discarded."
+			 */
+			if (ones_count != 5) {
+
+				/*
+				 * frequency shift decodes to '0'
+				 *
+				 * I can't find a document that explains NZRI anywhere
+				 * (as it pertains to amateur radio), but I saw it in a comment
+				 * in the BertOS source code, so I assume it's true :)
+				 */
+				bits >>= 1; /* insert a 0 from the left */
+				bits_count++;
+			}
+
+			/* clear shift_detect so we don't re-sample the same bit */
+			shift_detect = 0;
+
+			/*
+			 * Set the pseudo-timer to sample the next bit.
+			 *
+			 * If we are here, then the signal is at the leading edge of an
+			 * AFSK clock (since a shift was detected and this interrupt is
+			 * running 8 times faster than the AFSK baud rate of 1200).
+			 *
+			 * I want to make the next sample 1/1200 of a second from now,
+			 * but I want some wiggle room. For example, I don't want to
+			 * beat the AFSK clock to the next bit. Therefore, I will check
+			 * the state half way through the next AFSK clock cycle.
+			 *
+			 * 9600/1200 == 8, 8*1.5 == 12
+			 */
+			sample_timer = 12;
+
+			ones_count = 0; /* clear consecutive 1's counter */
+
+		} else {
+
+			/* when the time is right, sample! */
+			if (--sample_timer == 0) {
+
+				/* no frequency shift decodes to '1' */
+				bits = ((0x80) | (bits >> 1)); /* insert a 1 from the left */
+				bits_count++;
+
+				/* 9600/1200 == 8 */
+				sample_timer = 8;
+
+				ones_count++; /* keep a tally of consecutive 1's received */
+			}
+		}
+
+		/*
+		 * The only time there is supposed to be six '1' bits in a row is the
+		 * AX.25 flag (0b01111110). I use this property to synchronize the
+		 * 'bits_count' variable. That variable is used to know when to sample
+		 * the byte held in 'bits'.
+		 *
+		 * If the flag is found or the byte buffer is full, then send the byte
+		 * and reset the bits_count to 0.
+		 */
+		if ((bits == AX25_FLAG) || (bits_count >= 8 && in_kiss_frame == 1)) {
+
+
+			/*
+			 * When AX25_FLAG is received, it begins and/or ends an
+			 * AX.25 frame. KISS encapsulates AX.25 frames, so I need
+			 * to send an unescaped KISS_FEND byte with kiss_tx_raw()
+			 */
+			if (bits == AX25_FLAG) {
+
+				if (in_kiss_frame) {
+					/*
+					 * End the current KISS frame. I also send 'bits'
+					 * (the AX25_FLAG) before KISS_FEND to ensure that
+					 * the encapsulated frame ends with an AX25_FLAG. I
+					 * do this because there isn't a 1 to 1 relationship
+					 * between KISS_FENDS and AX25_FLAG. At best, this will
+					 * comply with the spec. At worse, it will send empty
+					 * AX.25 frames to the computer.
+					 */
+					kiss_tx(bits);
+					kiss_tx_raw(KISS_FEND);
+					sample_timer -= 2;
+					in_kiss_frame = 0;
+				}
+
+				/* start the next frame */
+				kiss_tx_raw(KISS_FEND);
+				sample_timer--;
+				in_kiss_frame = 1;
+			}
+
+			/*
+			 * Send the byte over USART0. Use kiss_tx() to ensure that
+			 * special characters are escaped properly.
+			 *
+			 * If more than 1 character is sent, the sample_time is
+			 * adjusted accordingly. This is because the USART transmit
+			 * is running at 9600 baud. If it has to wait to send a 2nd byte,
+			 * then that wait time needs to be subtracted from the sample_time.
+			 */
+			sample_timer -= (kiss_tx(bits) - 1);
+
+			/* clear bit counter */
+			bits_count = 0;
+		}
+
+	} else {
+
+		if (in_kiss_frame) {
+			/* End the current KISS frame. */
+			kiss_tx(AX25_FLAG);
+			kiss_tx_raw(KISS_FEND);
+			in_kiss_frame = 0;
+		}
+
+		/* Reset Internal State */
+		bits = 0x00;
+		bits_count = 0;
+		ones_count = 0;
+		sample_timer = 1;
+	}
 }
 
 /* capture the period of an input waveform */
